f4d51721b949ecccb45730d3d6ea712aa76d3112
FDRT94 <99046164@mydavinci.nl>
2020-02-18 10:47:22 +0100

    ADD show function for article and made article show template

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,15 +11,50 @@ class ArticleController extends AbstractController
 
 {
     /**
-     * @Route("/", methods={"GET"})
-     *
+     * @Route("/", methods={"GET"}, name="article_list")
      */
     public function index()
     {
-//        return new  Response('<html><body>hello</body></html>');
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
-        return $this->render("articles/index.html.twig");
+        return $this->render("articles/index.html.twig", array('articles' => $articles));
     }
+
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+    public function show($id)
+    {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        return $this->render('articles/show.html.twig', array('article' => $article));
+    }
+
+//
+//    /**
+//     * @Route("/article/save")
+//     */
+//    public function save()
+//    {
+//        /**
+//         * dont do this in live version
+//         * this is just for now
+//         * there should not be a get function for entity's when doing things with the database
+//         */
+//
+//        $entityManager = $this->getDoctrine()->getManager();
+//
+//        $article = new Article();
+//        $article->setTitle('Article 2');
+//        $article->setBody('This is the body for article two');
+//
+//        $entityManager->persist($article);
+//
+//        $entityManager->flush();
+//
+//        return new Response('Saves an article with the id of'.$article->getId());
+//    }
+
 
 
 }
