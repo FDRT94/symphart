f2e9b310e265e438039a2f31710c4c17739f5e6f
FDRT94 <99046164@mydavinci.nl>
2020-02-18 15:28:31 +0100

    add validator and csrf protection

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,8 +8,10 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -32,7 +34,7 @@ class ArticleController extends AbstractController
      * @Route("/article/new/" , name="new_article", methods={"GET","POST"})
      * add validator and csrf later
      * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     * @return RedirectResponse|Response
      */
     public function new(Request $request)
     {
@@ -60,15 +62,15 @@ class ArticleController extends AbstractController
             return $this->redirectToRoute('article_list');
         }
 
-            return $this->render('articles/new.html.twig', array('form' => $form->createView()
-            ));
-        }
+        return $this->render('articles/new.html.twig', array('form' => $form->createView()
+        ));
+    }
 
     /**
      * @Route("/article/edit/{id}" , name="edit_article", methods={"GET","POST"})
      * @param Request $request
      * @param $id
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     * @return RedirectResponse|Response
      */
     public function edit(Request $request, $id)
     {
@@ -77,15 +79,12 @@ class ArticleController extends AbstractController
         $form = $this->createFormBuilder($article)
             ->add('title', TextType::class, array('attr' =>
                 array('class' => 'form-control')))
-
             ->add('body', TextareaType::class, array(
                 'required' => false,
                 'attr' => array('class' => 'form-control')))
-
             ->add('save', SubmitType::class, array(
                 'label' => 'Update',
                 'attr' => array('class' => 'btn btn-primary mt-3')))
-
             ->getForm();
 
         $form->handleRequest($request);
@@ -106,53 +105,26 @@ class ArticleController extends AbstractController
      * @param $id
      * @return Response
      */
-        public function show($id)
-        {
-            $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+    public function show($id)
+    {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
-            return $this->render('articles/show.html.twig', array('article' => $article));
-        }
+        return $this->render('articles/show.html.twig', array('article' => $article));
+    }
 
     /**
      * @Route("/article/delete/{id}", methods={"DELETE"})
      * @param $id
      */
-        public function delete($id)
-        {
-            $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+    public function delete($id)
+    {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($article);
-            $entityManager->flush();
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($article);
+        $entityManager->flush();
 
-            $response = new Response();
-            $response->send();
-        }
-
-//
-//    /**
-//     * @Route("/article/save")
-//     */
-//    public function save()
-//    {
-//        /**
-//         * dont do this in live version
-//         * this is just for now
-//         * there should not be a get function for entity's when doing things with the database
-//         */
-//
-//        $entityManager = $this->getDoctrine()->getManager();
-//
-//        $article = new Article();
-//        $article->setTitle('Article 2');
-//        $article->setBody('This is the body for article two');
-//
-//        $entityManager->persist($article);
-//
-//        $entityManager->flush();
-//
-//        return new Response('Saves an article with the id of'.$article->getId());
-//    }
-
-
+        $response = new Response();
+        $response->send();
     }
+}
